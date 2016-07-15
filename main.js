9dff4855d821d30e1f8422f9ae67fadba28f0d86
Alyson Takai <shtakai@gmail.com>
2016-07-14 19:01:41 -0700

    implement simple callback

--- main.js
+++ main.js
@@ -2,10 +2,24 @@ console.log('start');
 
 let myApp = angular.module('myApp', []);
 
-myApp.factory('produtFactory', function(){
+function Product (name, price){
+  this.name = name;
+  this.price = new Number(price);
+}
+
+myApp.factory('productFactory', function(){
   console.log('factory start');
   let factory = {};
+  let productList = [];
+  productList.unshift(
+    new Product('test', 100.01)
+  );
+  console.log(productList);
 
+  factory.getProductList = function(callback){
+    console.log('invoke getProductList');
+    callback(productList);
+  }
 
 
   return factory;
@@ -14,8 +28,12 @@ myApp.factory('produtFactory', function(){
 
 myApp.controller('productsController',[
   '$scope',
-  'produtFactory',
-  function($scope, produtFactory){
+  'productFactory',
+  function($scope, productFactory){
     console.log('controller start');
+
+    productFactory.getProductList(function (data){
+      console.log(data);
+    })
   }
 ])
